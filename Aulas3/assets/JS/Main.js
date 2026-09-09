6e2c12206c8db03c3786b68c79071755f4ec92bf
walmirdev <walmir.neto@tnpromotora.com>
2026-09-08 22:15:46 -0300

    Organizando functions

--- Aulas3/assets/JS/Main.js
+++ Aulas3/assets/JS/Main.js
@@ -3,9 +3,7 @@ const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 
-fetch(url).then(function(response) {
-    console.log(response);
-})
-
-const x = 10+10;
-console.log(x);
+fetch(url)
+    .then((response) =>  response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
